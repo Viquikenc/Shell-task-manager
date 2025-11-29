42afaa0bcfa04b84527d1bb031a749eef290272b
Viquikenc <Newtiam@protonmail.com>
2025-11-29 21:01:04 +0100

    Feature: Writing Failures in log file

--- error_handler.c
+++ error_handler.c
@@ -4,35 +4,53 @@
 
 #include "error_handler.h"
 
+void write_log(char *message) {
+  FILE *err_file = fopen("errors.log", "a+");
+  fprintf(err_file, "%s\n\n", message);
+  fclose(err_file);
+}
 void err_set(err_enum err_code, const char *file, uint64_t line,
              const char *func) {
+  char message[256] = {0};
   switch (err_code) {
   case SUCCESS:
     break;
   case ERR_OPEN_FILE:
-    printf("[%s] Error in [%lu : %s] Opening File Failed\n", file, line, func);
-    printf("%s", strerror(errno));
+    sprintf(message, "[%s] Error in [%lu : %s] Opening File Failed\n-->%s",
+            file, line, func, strerror(errno));
+    printf("%s", message);
+    write_log(message);
     break;
   case ERR_SCAN_FILE:
-    printf("[%s] Error in [%lu : %s] Scaning File Failed\n", file, line, func);
-    printf("%s", strerror(errno));
+    sprintf(message, "[%s] Error in [%lu : %s] Scaning File Failed\n-->%s",
+            file, line, func, strerror(errno));
+    printf("%s", message);
+    write_log(message);
     break;
   case ERR_READ_FILE:
-    printf("[%s] Error in [%lu : %s] Reading File Failed\n", file, line, func);
-    printf("%s", strerror(errno));
+    sprintf(message, "[%s] Error in [%lu : %s] Reading File Failed\n-->%s",
+            file, line, func, strerror(errno));
+    printf("%s", message);
+    write_log(message);
     break;
   case ERR_PERMMISSION_DENIED:
-    printf("[%s] Error in [%lu : %s] Permission Denied\n", file, line, func);
-    printf("%s", strerror(errno));
+    sprintf(message, "[%s] Error in [%lu : %s] Permission Denied\n-->%s", file,
+            line, func, strerror(errno));
+    printf("%s", message);
+    write_log(message);
     break;
   case ERR_NOT_EXIST_FILE:
-    printf("[%s] Error in [%lu : %s] File or Directory Does Not Exist\n", file,
-           line, func);
-    printf("%s", strerror(errno));
+    sprintf(message,
+            "[%s] Error in [%lu : %s] File or Directory Does Not Exist\n-->%s",
+            file, line, func, strerror(errno));
+    printf("%s", message);
+    write_log(message);
     break;
   default:
-    printf("[%s] Unknown Error [%lu : %s]\n", file, line, func);
-    printf("%s", strerror(errno));
+    sprintf(message, "[%s] Unknown Error [%lu : %s] | %s\n", file, line, func,
+            strerror(errno));
+    printf("%s", message);
+    write_log(message);
     break;
   }
 }
